219a029d8118a08d21c960ee68e8b9475dcbae99
Moeinnnnnn <115925427+Moeinnnnnn@users.noreply.github.com>
2022-11-06 23:46:44 +0330

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+		
+	float a,b,c,d,f,g;
+	
+	cout <<"please enter 2 number with space :";
+	
+	cin >> a>>b;
+	
+	c=a+b;
+	
+    d=a-b;
+    
+	f=a*b;
+	
+	g=a/b;
+	
+	cout<<"+";
+	cout<<c<<endl;
+	
+	cout<<"-";
+	cout<<d<<endl;
+	
+	cout<<"*";
+	cout<<f<<endl;
+	
+	cout<<"/";
+	cout<<g;
+}
