6e77d68599d136d4c1c18bbd4ab481ac5358cfcd
Moeinnnnnn <115925427+Moeinnnnnn@users.noreply.github.com>
2022-11-07 00:45:11 +0330

    Add files via upload

---
+++ part 2 week4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	float x,y,z,a,b,c,d,e,f,g,h;
+	
+	cout<<"please enter 3 number with space and respectively for x,y,z : ";
+	
+	cin >> x >> y >> z;
+	
+	a=x-y;
+	
+	b=x+z;
+	
+	c=x+y+z;
+	
+	d=a*b;
+	
+	f=c*c;
+	
+	g=f+1;
+	
+	h=d/g;
+	
+	cout << h ;
+	
+}
